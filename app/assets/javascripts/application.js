dd596b0ef2972d506d473b9c4ebbf3b3742eea62
Raghu Betina <rbetina@github.com>
2015-07-07 13:42:44 -0500

    Initial commit

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,41 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+var map;
+
+function initGeolocation() {
+  if (navigator && navigator.geolocation) {
+    var watchId = navigator.geolocation.watchPosition(successCallback,
+                                                      errorCallback,
+                                                      {
+                                                        enableHighAccuracy: true,
+                                                        timeout: 60000,
+                                                        maximumAge: 0
+                                                      });
+
+  } else {
+    console.log('Geolocation is not supported');
+  }
+}
+
+function errorCallback() {}
+
+function successCallback(position) {
+  var myLatlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+  if(map == undefined) {
+    var myOptions = {
+      zoom: 15,
+      center: myLatlng,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+    map = new google.maps.Map(document.getElementById("map"), myOptions);
+  }
+  else map.panTo(myLatlng);
+
+  marker = new google.maps.Marker({
+    position: myLatlng,
+    map: map,
+    title: 'Current Position'
+  });
+}
